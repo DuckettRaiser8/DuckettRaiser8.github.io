0b425c0d9e59911a14419bbb044c39010ccb34e4
DuckettRaiser8 <lavar.duckett@jpschools.org>
2026-06-10 13:40:53 +0000

    update snake game

--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -10,6 +10,21 @@ $("<div>")
   .appendTo("#die");
 });
 
+function makeDot(top, left, elementID){
+  $("<div>")
+  .css("height", 15)
+  .css("width", 15)
+  .css("background-color", "black")
+  .css("position", "absolute")
+  .css("top", 50)
+  .css("left", 50)
+  .appendTo(top, left, elementID);
+}
+
+makeDot(50, 50, "#die"); // middle middle
+makeDot(25, 25, "#die"); // top left
+makeDot(75, 75, "#die"); // bottom right
+
 function rollDie(dieID){
   var randomNum = Math.ceil(Math.random() * 6);
   console.log(randomNum);
