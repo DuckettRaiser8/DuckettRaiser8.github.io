11a66989fa0d60f1649d5a7acc725b94e84f8114
DuckettRaiser8 <lavar.duckett@jpschools.org>
2026-06-23 16:17:19 +0000

    finsihed walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -36,6 +36,7 @@ function runProgram() {
   Note: You can have multiple event listeners for different types of events.
   */
   $(document).on("keydown", handleKeyDown);
+  $(document).on("keyup", handleKeyUp);
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -47,6 +48,7 @@ function runProgram() {
   */
   function newFrame() {
     repositionGameItem();
+    wallCollision();
     redrawGameItem();
   }
 
@@ -68,11 +70,26 @@ function runProgram() {
     }
     if (event.which === KEY.UP) {
       console.log("up pressed");
-      walker.speedY = 5;
+      walker.speedY = -5;
     }
     if (event.which === KEY.DOWN) {
       console.log("down pressed");
-      walker.speedY = -5;
+      walker.speedY = 5;
+    }
+  }
+
+  function handleKeyUp(event){
+     if (event.which === KEY.LEFT) {
+      walker.speedX = 0;
+    }
+    if (event.which === KEY.RIGHT) {
+      walker.speedX = 0;
+    }
+    if (event.which === KEY.UP) {
+      walker.speedY = 0;
+    }
+    if (event.which === KEY.DOWN) {
+      walker.speedY = 0;
     }
   }
 
@@ -98,6 +115,21 @@ function redrawGameItem(){
   $("#walker").css("left", walker.x);
   $("#walker").css("top", walker.y);
   console.log("Walker position:", walker.x, walker.y);
+}
+
+function wallCollision(){
+  if(walker.x > 380){
+      walker.x -= walker.speedX;
+  }
+   if(walker.x <= -5){
+    walker.x -= walker.speedX;
+  }
+   if(walker.y > 380){
+    walker.y -= walker.speedY;
+  }
+   if(walker.y <= -5){
+      walker.y -= walker.speedY;
+  }
 }
 
 }
