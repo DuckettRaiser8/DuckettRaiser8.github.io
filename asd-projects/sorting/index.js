dae0ca019792a34514d409798438c305585f782d
DuckettRaiser8 <lavar.duckett@jpschools.org>
2026-07-01 14:48:29 +0000

    saving sorting exercise

--- asd-projects/sorting/index.js
+++ asd-projects/sorting/index.js
@@ -29,39 +29,38 @@ async function bubbleSort(array) {
 
 // TODO 3: Implement quickSort
 
-async function quickSort(array, left, right){
-if(right - left > 0){
+async function quickSort(array, left, right) {
+  if (right - left > 0) {
     var index = await partition(array, left, right);
-    if(left < index - 1){
-        await quickSort(array, left, index - 1);
+    if (left < index - 1) {
+      await quickSort(array, left, index - 1);
     }
-    else if(right > index){
-        await quickSort(array, index, right);
+    if (right > index) {
+      await quickSort(array, index, right);
     }
-}
-else{
+  } else {
     return;
-}
+  }
 }
 
 // TODOs 4 & 5: Implement partition
 
-async function partition(array, left, right){
-var pivot = array[Math.floor((right + left) / 2)].value;
-while(left < right){
-while(array[left].value < pivot){
-    left++;
-}
-while(array[right].value > pivot){
-    right--;
-}
-if (left < right){
-    swap(array, left, right);
-    updateCounter(quickCounter);
-    await sleep();
-}
-}
-return left + 1;
+async function partition(array, left, right) {
+  var pivot = array[Math.floor((right + left) / 2)].value;
+  while (left < right) {
+    while (array[left].value < pivot) {
+      left++;
+    }
+    while (array[right].value > pivot) {
+      right--;
+    }
+    if (left < right) {
+      swap(array, left, right);
+      updateCounter(quickCounter);
+      await sleep();
+    }
+  }
+  return left + 1;
 }
 
 // TODO 1: Implement swap
