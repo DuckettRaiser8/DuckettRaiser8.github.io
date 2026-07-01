7828200835f33230423348ac6a1d6876ab3207df
DuckettRaiser8 <lavar.duckett@jpschools.org>
2026-07-01 14:37:36 +0000

    updating sorting project

--- asd-projects/sorting/index.js
+++ asd-projects/sorting/index.js
@@ -17,9 +17,9 @@ The CSS ids you will work with are:
 
 async function bubbleSort(array) {
   for (var i = 0; i <= array.length - 1; i++) {
-    for (var j = array[i].length - 1; j <= i + 1; j++) {
+    for (var j = array.length - 1; j >= i + 1; j--) {
       if (array[j].value < array[j - 1].value) {
-        swap(array, array[j], array[j - 1]);
+        swap(array, j, j - 1);
         updateCounter(bubbleCounter);
         await sleep();
       }
@@ -29,7 +29,40 @@ async function bubbleSort(array) {
 
 // TODO 3: Implement quickSort
 
+async function quickSort(array, left, right){
+if(right - left > 0){
+    var index = await partition(array, left, right);
+    if(left < index - 1){
+        await quickSort(array, left, index - 1);
+    }
+    else if(rigjt > index){
+        await quickSort(array, index, right);
+    }
+}
+else{
+    return;
+}
+}
+
 // TODOs 4 & 5: Implement partition
+
+async function partition(array, left, right){
+var pivot = array[Math.floor((right + left) / 2)].value;
+while(left < right){
+while(array[left].value < pivot){
+    left++;
+}
+while(array[right].value > pivot){
+    right--;
+}
+if (left < right){
+    swap(array, left, right);
+    updateCounter(quickCounter);
+    await sleep();
+}
+}
+return left + 1;
+}
 
 // TODO 1: Implement swap
 
